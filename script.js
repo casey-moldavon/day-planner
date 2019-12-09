2bd588918230ea07dc7fedb137534170f98fad3c
Casey Yang <cmoldavon@gmail.com>
2019-12-08 16:39:42 -0800

    Finishing initial styling in style.css

---
+++ script.js
@@ -0,0 +1,8 @@
+// self note: create values for each textarea
+// create variables calling each textarea and each connecting button
+// add event listeners for each button to oull value from textarea
+// save value to local storage
+// display local storaged data in textarea (?)
+
+
+var 
