11203a7d8f5a211496857fd15d663bc2a4e95f0a
Casey Yang <cmoldavon@gmail.com>
2019-12-08 21:30:03 -0800

    Updating sctyle.css and adding notes to script.js

--- script.js
+++ script.js
@@ -4,5 +4,21 @@
 // save value to local storage
 // display local storaged data in textarea (?)
 
+//used to start Jquery
+$(document).ready(function () {
+
+
+
+
+
+    //if statement:
+    //if container-#am/pm = current current hour, change textarea to red
+    //$("9am-textarea").css({color: "red"});
+    //also change time-row font color to match the text box (personal extra feature)
 
 var 
+
+
+
+
+});
