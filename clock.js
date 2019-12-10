ef3055f1491f1904ab18c4d35902f97a3470a187
Casey Yang <cmoldavon@gmail.com>
2019-12-09 21:43:21 -0800

    Adding cloakc.js which contains complete Date/Time

---
+++ clock.js
@@ -0,0 +1,38 @@
+    //------------------------- THE CLOCK!!!!! -------------------------
+    function displayTime(){
+        var mydate = new Date();
+        var year = mydate.getFullYear();
+            if(year < 1000){
+                year += 1900
+            }
+            var day = mydate.getDay();
+            var month = mydate.getMonth();
+            var daym = mydate.getDate();
+            var dayArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
+            var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var date = dayArray[day] + " " + daym + " " + monthArray[month] + " " + year //string display of complete date
+
+            var currentTime = new Date();
+
+            var hours = currentTime.getHours();
+            var minutes = currentTime.getMinutes();
+            var seconds = currentTime.getSeconds();
+            var time = hours + ":" + minutes + ":" + seconds //string display of complete time
+
+
+
+            if (hours === 24) {hours = 0;}
+            else if (hours > 12) {hours = hours - 0;}
+            if (hours < 10) {hours = "0" + hours;}
+            if (minutes < 10) {minutes = "0" + minutes;}
+            if (seconds < 10) {seconds = "0" + seconds;}
+
+            var myClock = document.getElementById("time-display");
+            myClock.textContent = "" + date + " | " + time
+            myClock.innerText = "" + date + " | " + time
+
+            setTimeout("displayTime()", 1000);
+    }
+
+    //calling the clock function so that it continuously updates the display on-screen
+    displayTime();
--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+// $(document).ready(function() {
+    //used to start Jquery
+
+
+    //------------------------- Current Time highlights -------------------------
+    //this section contains the code directed at altering the color for each textarea...
+    //based on if the current hour is equal to, before or after that of the textarea's.
+
+    var nineAm = document.getElementById("9am-textarea");
+    var tenAm = document.getElementById("10am-textarea");
+    var elevenAm = document.getElementById("11am-textarea");
+    var twelvePm = document.getElementById("12pm-textarea");
+    var onePm = document.getElementById("1pm-textarea");
+    var twoPm = document.getElementById("2pm-textarea");
+    var threePm = document.getElementById("3pm-textarea");
+    var fourPm = document.getElementById("4pm-textarea");
+    var fivePm = document.getElementById("5pm-textarea");
+
+    function timeHighlight(){
+        //9am textarea
+        if (hours >)
+    }
+
+// });
+
+
+
 // self note: create values for each textarea
 // create variables calling each textarea and each connecting button
-// add event listeners for each button to oull value from textarea
+// add event listeners for each button to pull value from textarea
 // save value to local storage
 // display local storaged data in textarea (?)
-
-//used to start Jquery
-$(document).ready(function () {
 
 
 
@@ -14,13 +38,11 @@ $(document).ready(function () {
     //if statement:
     //if container-#am/pm = current current hour, change textarea to red
     //$("9am-textarea").css({color: "red"});
+    //personal note: possibly use military time to be more precise with conditionals
     //also change time-row font color to match the text box (personal extra feature)
 
     //possibly design feature: after saved to local stroage, all text that has been saved is made bold
 
-var 
 
 
 
-
-});
